e804631ff4cdd7f83a979daf0836fb9cd584aa10
glenpaulsonv <glenpaulsonv@gmail.com>
2021-04-23 09:22:21 +0530

    delete_record
    Deleting the record corresponding to the reg.no.

---
+++ PHP and MYSQL CRUD Operations/delete_record/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Deleting the Record</title>
+</head>
+<body>
+    <!-- Delete the record corresponding to the reg.no.-->
+    <form action="delete_record.php" method="GET">
+    Reg. No. <input type="text" name="r"> <br><br>
+    <input type="submit" value="Delete">
+    </form>
+</body>
+</html>
