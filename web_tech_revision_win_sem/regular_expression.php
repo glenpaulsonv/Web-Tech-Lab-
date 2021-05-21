2182d2213ac3931454cdef72220543fc63d3aed0
glenpaulsonv <glenpaulsonv@gmail.com>
2021-05-21 08:22:26 +0530

    htmlentities
    using htmlentities to display the html tags in the form entries as it is.

--- web_tech_revision_win_sem/regular_expression.php
+++ web_tech_revision_win_sem/regular_expression.php
@@ -4,18 +4,23 @@ $v = "";
 
 if(isset($_POST['n']))
 {
-    $v = $_POST['n'];
+    $v = htmlentities($_POST['n']);
     
 }
 
 
 ?>
 
+<!--HTML Entities are used so that html tags in the form entry are displayed as it is-->
+
 <form action="" method="POST">
     <input type="text" name="n">
     <input type="submit">
 
 </form>
+
+<!--<script>alert('u r hacked')</script>-->
+
 
 <?php
 
